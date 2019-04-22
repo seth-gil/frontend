d06796781921fd844d18a62b86916649336f4fa6
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-04-22 19:15:20 +0100

    Message: Changed API root

---
+++ build/precache-manifest.f7b874a24426dad4ff76878f19529fb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9feaa7047cc4737612eb",
+    "url": "/static/js/main.a215c11b.chunk.js"
+  },
+  {
+    "revision": "42f5b08b7ce14db5d1e6",
+    "url": "/static/js/2.93302569.chunk.js"
+  },
+  {
+    "revision": "42f5b08b7ce14db5d1e6",
+    "url": "/static/css/2.56ec7d2a.chunk.css"
+  },
+  {
+    "revision": "0663e86b2e612888e7a838f1e2a21c20",
+    "url": "/index.html"
+  }
+];
